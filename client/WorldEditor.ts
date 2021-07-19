3afbcf8380eebf9f5089556638946d475ea32455
waleed alqallaf <waleedalqalaf2@gmail.com>
2021-07-19 23:50:01 +0300

    added changing tile and always placing even if u dont click e

--- client/WorldEditor.ts
+++ client/WorldEditor.ts
@@ -15,52 +15,83 @@ export class WorldEditor extends ClientGameObject {
 
     private cursorLocation: Vector2 = new Vector2(13, 13);
     private currentTile: string = 't';
+    private changeCurrentTileMode: boolean = false;
+    private alwaysPlace: boolean = false;
 
     ready() {
         keyboard.addKeyDownListener(this, (ev) => {
-            if(ev.key.toLowerCase() == "x") {
-                keyboard.claimOwnership(this);
+            if(this.changeCurrentTileMode) {
+                this.currentTile = ev.key;
+                this.changeCurrentTileMode = false;
+            } else {
+                if(ev.key.toLowerCase() == "x") {
+                    keyboard.claimOwnership(this);
+                }
+                switch(ev.key.toLowerCase()) {
+                    case "x": {
+                        this.isEditing = true;
+                        break;
+                    }
+                    case "f": {
+                        this.isEditing = false;
+                        break;
+                    }
+                    case "e": {
+                        this.alwaysPlace = !this.alwaysPlace;
+                        if(this.alwaysPlace) {
+                            this.emit("place", {
+                                x: this.cursorLocation.x,
+                                y: this.cursorLocation.y,
+                                z: renderer.cameraPosition.z,
+                                tile: this.currentTile
+                            } as PlaceTileData);
+                        }
+                        break;
+                    }
+                    case " ": {
+                        this.changeCurrentTileMode = true;
+                        break;
+                    }
+    
+                }
             }
-            switch(ev.key.toLowerCase()) {
-                case "x": {
-                    this.isEditing = true;
-                    break;
-                }
-                case "f": {
-                    this.isEditing = false;
-                    break;
-                }
-                case "e": {
-                    this.emit("place", {
-                        x: this.cursorLocation.x,
-                        y: this.cursorLocation.y,
-                        z: renderer.cameraPosition.z,
-                        tile: this.currentTile
-                    } as PlaceTileData);
-                    break;
-                }
-            }
+            
         });
     }
 
     update() {
         if(this.isEditing) {
             keyboard.startScope(this);
+            var moved = false;
+
             if (keyboard.isKeyDown("d") || keyboard.isKeyDown("D")) {
                 this.cursorLocation.x += 1;
+                moved = true;
             }
     
             if (keyboard.isKeyDown("a") || keyboard.isKeyDown("A")) {
                 this.cursorLocation.x -= 1;
+                moved = true;
             }
     
             if (keyboard.isKeyDown("s") || keyboard.isKeyDown("S")) {
                 this.cursorLocation.y += 1;
+                moved = true;
             }
     
             if (keyboard.isKeyDown("w") || keyboard.isKeyDown("W")) {
                 this.cursorLocation.y -= 1;
+                moved = true;
             }
+
+            if(this.alwaysPlace && moved)
+                this.emit("place", {
+                    x: this.cursorLocation.x,
+                    y: this.cursorLocation.y,
+                    z: renderer.cameraPosition.z,
+                    tile: this.currentTile
+                } as PlaceTileData);
+
             keyboard.endScope();
         }
         
@@ -69,7 +100,8 @@ export class WorldEditor extends ClientGameObject {
 
     draw() {
         if (this.isEditing){
-            renderer.setTile(this.cursorLocation.x, this.cursorLocation.y, 0, 'x');
+            renderer.setTile(this.cursorLocation.x, this.cursorLocation.y, 0, 
+                this.changeCurrentTileMode ? "C" : (this.alwaysPlace ? "P" : "X"));
 
         }
     }
