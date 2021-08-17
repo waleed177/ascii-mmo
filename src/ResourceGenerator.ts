63a7b91508f39ff165f16462dae3d1228129ecfe
waleed alqallaf <waleedalqalaf2@gmail.com>
2021-08-17 23:36:58 +0300

    added preamble

--- src/ResourceGenerator.ts
+++ src/ResourceGenerator.ts
@@ -1,9 +1,27 @@
+//#region PREAMBLE
+/*
+    This is an ASCII MMO game.
+    Copyright (C) 2021 waleed177 <potatoxel@gmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, version 3 of the
+    License only.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+//#endregion
+
 import { ItemData } from '../client/shared/Item';
 import { PrefabName } from '../client/shared/Prefabs';
 import { Chest } from './Chest';
 import { NetworkEntity } from './NetworkEntity';
-
-
 
 export class ResourceGenerator extends NetworkEntity {
 
